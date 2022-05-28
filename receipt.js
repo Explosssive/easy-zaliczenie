44b8249a8383c2c14a0635ae5db6c9551096f040
Explosssive <Creeper>
2022-05-28 18:33:16 +0200

    Receipts fixes

--- receipt.js
+++ receipt.js
@@ -1,9 +1,9 @@
 $(function(){
     $('#get-receipt').on("click", function(){
-        domtoimage.toPng(document.getElementById('solving'))
+        domtoimage.toPng(document.getElementById('solving'), {'bgcolor':'white'})
         .then(function (dataUrl) {
             var link = document.createElement('a');
-            link.download = 'receipt.jpg';
+            link.download = 'receipt.png';
             link.href = dataUrl;
             link.click();
         });
